7db01d7ba0523e5e64ca5fb2a6fde32274853ef6
VB <Vladimir.Bulat@gmail.com>
2024-03-04 14:36:00 +0300

    Lesson_01 ( Create app Factorial)

--- app/src/main/java/ru/com/vbulat/factorialtest/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/factorialtest/MainActivity.kt
@@ -2,10 +2,55 @@ package ru.com.vbulat.factorialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import ru.com.vbulat.factorialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        observeViewModel()
+
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel(){
+        viewModel.progress.observe(this) {
+            if (it){
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+
+        viewModel.error.observe(this) {
+            if (it) {
+                Toast.makeText(
+                    this,
+                    "You didn't entered value!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        viewModel.factorial.observe(this) {
+            binding.tvFactorial.text = it
+        }
     }
 }
